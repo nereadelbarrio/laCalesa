0a0a12e8a795c845f46ffb8011d1412e7daa4edd
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2020-12-14 17:17:56 +0100

    Update style.js

--- style.js
+++ style.js
@@ -29,7 +29,8 @@
 
 				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-1' } },
 				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-2' } },
-				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-3' } }
+				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-3' } },
+				{ name: 'Caja 4', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-4' } }
 			]
 		},
 
